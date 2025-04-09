fb446c4da02cdbbee4f60463c702578c0ed8ca89
Niklas Haiden <niklas.haiden@gmail.com>
2025-04-09 15:27:47 +0200

    add devcontainer json file

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import {pgTable, timestamp, uuid, varchar} from "drizzle-orm/pg-core";
+import { z } from "zod";
 
 export const pasteTable = pgTable("pastes", {
     id: uuid("id").primaryKey().defaultRandom().unique(),
@@ -6,3 +7,12 @@ export const pasteTable = pgTable("pastes", {
     language: varchar(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
 })
+
+export const pasteSchema = z.object({
+    id: z.string().uuid().optional(),
+    content: z.string(),
+    language: z.string(),
+    createdAt: z.date().optional(),
+});
+
+export type Paste = z.infer<typeof pasteSchema>;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Textarea } from '@/components/ui/textarea.tsx'
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from '@/components/ui/button.tsx'
+import { createServerFn } from '@tanstack/react-start'
+import fs from 'node:fs'
+import { type Paste, pasteSchema } from '@/db/schema.ts'
+
+const insertPaste = createServerFn({ method: 'POST' })
+  .validator((newPaste: Paste) => pasteSchema.parse(newPaste))
+  .handler(async ({ data: Paste }) => {
+
+  })
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -8,14 +17,16 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <main className={'w-dvw h-dvh flex flex-col items-center justify-center gap-5'}>
+    <main
+      className={'w-dvw h-dvh flex flex-col items-center justify-center gap-5'}
+    >
       <h1 className={'text-2xl font-bold'}>Paste your content here.</h1>
       <div className={'w-2/3 min-h-[500px]'}>
-        <Textarea className={"w-full h-full"} />
+        <Textarea className={'w-full h-full'} />
       </div>
-        <div className={"w-2/3"}>
-            <Button>Upload Paste</Button>
-        </div>
+      <div className={'w-2/3'}>
+        <Button>Upload Paste</Button>
+      </div>
     </main>
   )
 }

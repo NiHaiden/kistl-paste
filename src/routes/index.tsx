8a80127691cf6706afc8dcaa36795fcedf20db84
Niklas Haiden <niklas.haiden@gmail.com>
2025-04-19 22:58:17 +0200

    Add paste content state and upload functionality in `index.tsx` and export `db` in `db/index.ts`.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+import {useState} from "react";
+import type {Paste} from '@/db/schema.ts';
+import { pasteTable} from '@/db/schema.ts';
 import { Textarea } from '@/components/ui/textarea.tsx'
 import { Button } from '@/components/ui/button.tsx'
-import { createServerFn } from '@tanstack/react-start'
-import fs from 'node:fs'
-import { type Paste, pasteSchema } from '@/db/schema.ts'
+import {  pasteSchema } from '@/db/schema.ts'
+import { db } from '@/db'
 
 const insertPaste = createServerFn({ method: 'POST' })
   .validator((newPaste: Paste) => pasteSchema.parse(newPaste))
-  .handler(async ({ data: Paste }) => {
+  .handler(async ({ data }) => {
+    await db.insert(pasteTable).values(data);
 
   })
 
@@ -16,16 +20,25 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
+
+  const [pasteContent, setPasteContent] = useState<string>('')
+
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setPasteContent(event.target.value)
+  }
+  
   return (
     <main
       className={'w-dvw h-dvh flex flex-col items-center justify-center gap-5'}
     >
       <h1 className={'text-2xl font-bold'}>Paste your content here.</h1>
       <div className={'w-2/3 min-h-[500px]'}>
-        <Textarea className={'w-full h-full'} />
+        <Textarea className={'w-full h-full'} onChange={handleTextareaChange} />
       </div>
       <div className={'w-2/3'}>
-        <Button>Upload Paste</Button>
+        <Button onClick={() => insertPaste({data: {content: pasteContent, language: "js"}})}>Upload Paste</Button>
       </div>
     </main>
   )
